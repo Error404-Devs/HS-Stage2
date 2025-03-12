b4de9faae7a9cbcd9190331956ea84a2a29ab80a
David Rotariu <davidrotariu139@gmail.com>
2025-03-12 13:42:22 +0200

    Added login and signup html files

--- flask-app/app/frontend/routes.py
+++ flask-app/app/frontend/routes.py
@@ -18,3 +18,33 @@ def index():
 
     # Pass the message to the template
     return render_template("index.html", message=api_data["message"])
+
+
+@frontend_bp.route("/login", methods=["GET", "POST"])
+def login():
+    # if request.method == "POST":
+    #     username = request.form.get("username")
+    #     password = request.form.get("password")
+
+    #     # Make a request to your API backend for authentication
+    #     response = requests.post(
+    #         "http://127.0.0.1:5000/api/login",
+    #         json={"username": username, "password": password},
+    #     )
+
+    #     if response.status_code == 200:
+    #         session["user"] = username  # Store the user session
+    #         flash("Login successful!", "success")
+    #         return redirect(
+    #             url_for("frontend_bp.index")
+    #         )  # Redirect to home page or dashboard
+    #     else:
+    #         flash("Invalid credentials. Please try again.", "danger")
+
+    return render_template("login.html")
+
+
+@frontend_bp.route("/signup", methods=["GET", "POST"])
+def signup():
+
+    return render_template("signup.html")
